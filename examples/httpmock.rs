d34fcbc9316f133f1b1cae783b8e6a1b6dcb6c13
Kai <simaow@outlook.com>
2022-03-15 00:11:50 +1100

    add more on httpmock

--- examples/httpmock.rs
+++ examples/httpmock.rs
@@ -4,9 +4,16 @@ async fn main() {}
 #[cfg(test)]
 mod test {
     use super::*;
+    use serde::Deserialize;
+    use std::collections::HashMap;
     use tokio::sync::OnceCell;
     use wiremock::matchers::{method, path};
     use wiremock::{Mock, MockServer, ResponseTemplate};
+
+    #[derive(Debug, serde::Deserialize)]
+    pub struct Account {
+        name: String,
+    }
 
     static HTTP_SERVER: OnceCell<MockServer> = OnceCell::const_new();
 
@@ -17,13 +24,37 @@ mod test {
 
         Mock::given(path("/account"))
             .and(method("GET"))
-            .respond_with(ResponseTemplate::new(200))
+            .respond_with(
+                ResponseTemplate::new(200).set_body_raw(
+                    r#"
+                {"name": "testAccount"}
+                "#
+                    .as_bytes()
+                    .to_owned(),
+                    "application/json",
+                ),
+            )
             .mount(&http_server)
             .await;
 
         Mock::given(path("/contact"))
             .and(method("GET"))
-            .respond_with(ResponseTemplate::new(400))
+            .respond_with(
+                ResponseTemplate::new(400).set_body_raw(
+                    r#"
+                {"first name": "eric", "last name": "wang"}
+                "#
+                    .as_bytes()
+                    .to_owned(),
+                    "application/json",
+                ),
+            )
+            .mount(&http_server)
+            .await;
+
+        Mock::given(path("/account"))
+            .and(method("POST"))
+            .respond_with(ResponseTemplate::new(200))
             .mount(&http_server)
             .await;
 
@@ -34,28 +65,46 @@ mod test {
     async fn test_http_get_account_200() {
         let http_server = HTTP_SERVER.get_or_init(setup).await;
 
-        let res = reqwest::Client::new()
-            .get(&format!("{}/account", &http_server.uri()))
-            .send()
+        let res: Account = reqwest::get(&format!("{}/account", &http_server.uri()))
+            .await
+            .unwrap()
+            .json::<Account>()
             .await
             .unwrap();
 
-        println!("test_http_get_account_200 is {:?}", res);
-
-        assert_eq!(res.status().as_u16(), 200);
+        assert_eq!(res.name, "testAccount");
     }
 
     #[tokio::test]
     async fn test_http_get_contact_400() {
         let http_server = HTTP_SERVER.get_or_init(setup).await;
 
-        let res = reqwest::Client::new()
+        let res: HashMap<String, String> = reqwest::Client::new()
             .get(&format!("{}/contact", &http_server.uri()))
+            .send()
+            .await
+            .unwrap()
+            .json::<HashMap<String, String>>()
+            .await
+            .unwrap();
+
+        assert_eq!(
+            res.get(&"first name".to_string()),
+            Some(&"eric".to_string())
+        );
+    }
+
+    #[tokio::test]
+    async fn test_http_post_account_200() {
+        let http_server = HTTP_SERVER.get_or_init(setup).await;
+
+        let res = reqwest::Client::new()
+            .post(&format!("{}/account", &http_server.uri()))
             .send()
             .await
             .unwrap();
 
-        println!("test_http_get_contact_400 is {:?}", res);
-        assert_eq!(res.status().as_u16(), 400);
+        println!("test_http_post_account_200 is {:?}", res);
+        assert_eq!(res.status().as_u16(), 200);
     }
 }
