db1d5244532d223626421599c70675f6bfa3745e
Kai <simaow@outlook.com>
2022-03-18 23:10:01 +1100

    combine

--- examples/soql_parser.rs
+++ examples/soql_parser.rs
@@ -1,12 +1,12 @@
 use nom::{
     branch::alt,
     bytes::complete::{tag, tag_no_case, take},
-    character::complete::{alpha1, alphanumeric1, one_of, multispace0},
-    combinator::{ opt, flat_map }, 
+    character::complete::{alpha1, alphanumeric1, multispace0, one_of},
+    combinator::{flat_map, opt, peek},
     error::{context, ErrorKind, VerboseError},
     multi::{count, many0, many1, many_m_n},
     sequence::{preceded, separated_pair, terminated, tuple},
-    AsChar, Err as NomErr, IResult, InputTakeAtPosition
+    AsChar, Err as NomErr, IResult, InputTakeAtPosition,
 };
 
 /// https://github.com/Geal/nom/blob/main/doc/choosing_a_combinator.md
@@ -18,15 +18,15 @@ type Res<T, U> = IResult<T, U, VerboseError<T>>;
 #[derive(Debug, PartialEq, Eq)]
 pub struct SOQL {
     begin: OpenCloseSymbol,
-    selectclause: SelectClause,
-    fromclause: FromClause,
-    whereclause: WhereClause
+    selectclause: SOQLClause,
+    fromclause: SOQLClause,
+    whereclause: SOQLClause,
 }
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum OpenCloseSymbol {
     OPEN,
-    CLOSE
+    CLOSE,
 }
 
 impl From<&str> for OpenCloseSymbol {
@@ -34,70 +34,70 @@ impl From<&str> for OpenCloseSymbol {
         match i {
             "[" => OpenCloseSymbol::OPEN,
             "]" => OpenCloseSymbol::CLOSE,
-            _ => unimplemented!("not supported")
+            _ => unimplemented!("not supported"),
         }
     }
 }
 
 #[derive(Debug, PartialEq, Eq)]
-pub enum SelectClause {
-    SELECT
+pub enum SOQLClause {
+    SELECT,
+    FROM,
+    WHERE,
 }
 
-impl From<&str> for SelectClause {
+impl From<&str> for SOQLClause {
     fn from(i: &str) -> Self {
-        SelectClause::SELECT
+        println!("start from: {:?}", i);
+        match i.to_lowercase().as_str() {
+            "select" => SOQLClause::SELECT,
+            "from" => SOQLClause::FROM,
+            "where" => SOQLClause::WHERE,
+            _ => unimplemented!("not supported"),
+        }
     }
 }
 
-#[derive(Debug, PartialEq, Eq)]
-pub enum FromClause {
-    FROM
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub enum WhereClause {
-    WHERE
-}
-
-
 pub fn openclosesymbol(input: &str) -> Res<&str, OpenCloseSymbol> {
-    context("openclosesymbol", 
-        alt((tag_no_case("["), tag_no_case("]"))),
-    )(input)
-    .map(|(next_input, res)| (next_input, res.into()))
+    context("openclosesymbol", alt((tag_no_case("["), tag_no_case("]"))))(input)
+        .map(|(next_input, res)| (next_input, res.into()))
 }
 
-pub fn select(input: &str) -> Res<&str, SelectClause> {
-    let (next_input, _) = multispace0(input)?;
-    let (next_input, res) = tag_no_case("select")(next_input).map(|(next_input, res) | (next_input, res.into()))?;
-    let (next_input, _) = multispace0(next_input)?;
+pub fn select(input: &str) -> Res<&str, SOQLClause> {
+    let (r, (z, y, x)) = tuple((multispace0, tag_no_case("select"), multispace0))(input)?;
 
-    Ok((next_input, res))
+    println!("{}-{}-{}-{}", x, y, z, r);
+    Ok((r, y.into()))
+    // let (next_input, _) = multispace0(input)?;
+    // let (next_input, res) =
+    //     tag_no_case("select")(next_input).map(|(next_input, res)| (next_input, res.into()))?;
+    // let (next_input, _) = multispace0(next_input)?;
+
+    // Ok((next_input, res))
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use nom:: {
+    use nom::{
         error::{ErrorKind, VerboseError, VerboseErrorKind},
         Err as NomErr,
     };
 
     #[test]
     fn test_openclosesymbol() {
-        claim::assert_ok_eq!(openclosesymbol("[SELECT"), ("SELECT", OpenCloseSymbol::OPEN));
+        claim::assert_ok_eq!(
+            openclosesymbol("[SELECT"),
+            ("SELECT", OpenCloseSymbol::OPEN)
+        );
         claim::assert_ok_eq!(openclosesymbol("]"), ("", OpenCloseSymbol::CLOSE));
         claim::assert_err!(openclosesymbol("SELECT"));
     }
 
-
     #[test]
     fn test_select() {
-        claim::assert_ok_eq!(select("  \nSELECT  "), ("", SelectClause::SELECT));
-        claim::assert_ok_eq!(select("sElecT"), ("", SelectClause::SELECT));
+        claim::assert_ok_eq!(select("sElecT"), ("", SOQLClause::SELECT));
         claim::assert_err!(select("[SELECT"));
-        claim::assert_ok_eq!(select("SELECT a"), ("a", SelectClause::SELECT));
+        claim::assert_ok_eq!(select("  SELECT a"), ("a", SOQLClause::SELECT));
     }
 }
